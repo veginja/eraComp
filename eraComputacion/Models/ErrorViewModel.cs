4a3cac6d90413476b4485a03513ce47d86d472d4
veginja <49924996+veginja@users.noreply.github.com>
2020-01-05 23:26:08 -0600

    terminado

---
+++ eraComputacion/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eraComputacion.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
